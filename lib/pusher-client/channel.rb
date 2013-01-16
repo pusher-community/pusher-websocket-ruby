4a5dd4c34885f74a2587c1b275cb5826b9de71db
Scott Klein <scott@scottkle.in>
2013-01-16 15:11:43 -0700

    fix client behavior with modified global behavior

--- lib/pusher-client/channel.rb
+++ lib/pusher-client/channel.rb
@@ -2,7 +2,7 @@ module PusherClient
 
   class Channel
     attr_accessor :global, :subscribed
-    attr_reader :name, :callbacks, :global_callbacks, :user_data
+    attr_reader :name, :callbacks, :user_data
 
     def initialize(channel_name, user_data=nil, logger=PusherClient.logger)
       @name = channel_name
@@ -10,11 +10,11 @@ module PusherClient
       @logger = logger
       @global = false
       @callbacks = {}
-      @global_callbacks = {}
       @subscribed = false
     end
 
     def bind(event_name, &callback)
+      PusherClient.logger.debug "Binding #{event_name} to #{name}"
       @callbacks[event_name] = callbacks[event_name] || []
       @callbacks[event_name] << callback
       return self
@@ -22,28 +22,16 @@ module PusherClient
 
     def dispatch_with_all(event_name, data)
       dispatch(event_name, data)
-      dispatch_global_callbacks(event_name, data)
     end
 
     def dispatch(event_name, data)
-      logger.debug("Dispatching callbacks for #{event_name}")
+      logger.debug("Dispatching #{global ? 'global ' : ''}callbacks for #{event_name}")
       if @callbacks[event_name]
         @callbacks[event_name].each do |callback|
           callback.call(data)
         end
       else
-        logger.debug("No callbacks to dispatch for #{event_name}")
-      end
-    end
-
-    def dispatch_global_callbacks(event_name, data)
-      if @global_callbacks[event_name]
-        logger.debug("Dispatching global callbacks for #{event_name}")
-        @global_callbacks[event_name].each do |callback|
-          callback.call(data)
-        end
-      else
-        logger.debug("No global callbacks to dispatch for #{event_name}")
+        logger.debug "No #{global ? 'global ' : ''}callbacks to dispatch for #{event_name}"
       end
     end
 
